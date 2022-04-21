6fccf9a9bb2291db2bdd31277be5eb930c0599a2
Adarsha Khatua <97473442+adarshakhatua@users.noreply.github.com>
2022-04-21 09:21:18 +0530

    Add files via upload

---
+++ unit5-styled component/src/App.jsx
@@ -0,0 +1,17 @@
+import {Button} from './components/button';
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <Button type="primary">Primary Button</Button>
+      <Button type="default">Default Button</Button>
+      <Button type="dashed">Dashed Button</Button>
+      <Button type="text">Text Button</Button>
+      <Button type="link">Link Button</Button>
+    </div>
+  );
+}
+
+export default App;
